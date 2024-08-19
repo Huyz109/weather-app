6b9d658861a8d2496b7f4656e16291e77ca1af8e
Huyz109 <phamqhuy2001@gmail.com>
2024-08-19 21:59:30 +0700

    add search bar

--- app/home.tsx
+++
@@ -1,9 +0,0 @@
-import { SafeAreaView, Text } from "react-native";
-
-export default function Home() {
-    return (
-        <SafeAreaView>
-            <Text>Home screen</Text>
-        </SafeAreaView>
-    )
-}
